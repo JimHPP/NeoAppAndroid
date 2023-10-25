11256ffd3441727cdd701b40c22fc695806a5efb
JimHPP <haroldead.25@gmail.com>
2023-10-25 17:56:26 -0500

    Add: App-In Message Firebase

--- app/src/main/java/com/neo/neokm/MainActivity.kt
+++ app/src/main/java/com/neo/neokm/MainActivity.kt
@@ -3,8 +3,10 @@ package com.neo.neokm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
+import com.google.firebase.installations.FirebaseInstallations
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val btnEnviar = findViewById<AppCompatButton>(R.id.btnEnviar)
         val eTName = findViewById<AppCompatEditText>(R.id.eTName)
+        logInstallationID()
         btnEnviar.setOnClickListener {
             if(eTName.text.toString().isNotEmpty()){
                 val intent = Intent(this, ResultActivity::class.java)
@@ -19,5 +22,16 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
+    }
+    private fun logInstallationID() {
+        // [START get_installation_id]
+        FirebaseInstallations.getInstance().id.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.d("Installations", "Installation ID: " + task.result)
+            } else {
+                Log.e("Installations", "Unable to get Installation ID")
+            }
+        }
+        // [END get_installation_id]
     }
 }
