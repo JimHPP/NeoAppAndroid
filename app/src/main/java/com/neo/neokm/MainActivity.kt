8cd5cf833736896dc4810165479a06417c5b4ef4
JimHPP <haroldead.25@gmail.com>
2023-10-25 14:28:07 -0500

    Add: Analytics Firebase

--- app/src/main/java/com/neo/neokm/MainActivity.kt
+++ app/src/main/java/com/neo/neokm/MainActivity.kt
@@ -5,14 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseAnalytics = Firebase.analytics
         val btnEnviar = findViewById<AppCompatButton>(R.id.btnEnviar)
         val eTName = findViewById<AppCompatEditText>(R.id.eTName)
         btnEnviar.setOnClickListener {
+            firebaseAnalytics.logEvent("virtualEventApp") {
+                param("Zona", "Inicio")
+                param("TipoEvento", "Click")
+                param("TipoElemento", "Botón")
+                param("Valor", btnEnviar.text.toString())
+            }
             if(eTName.text.toString().isNotEmpty()){
                 val intent = Intent(this, ResultActivity::class.java)
                 intent.putExtra("EXTRA_NAME", eTName.text.toString())
